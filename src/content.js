e921fe3399b63d306f4cf5482312c51514e0deb6
Talisson <talissonlux@outlook.com>
2020-09-19 21:37:49 -0300

    Added menu items, function to save playbackRate

--- src/content.js
+++ src/content.js
@@ -1,13 +1,13 @@
 const interval = setInterval (() => {
     const header = document.querySelector('._3All_._3NrAe span')
     if(header) {
-        /* console.log(header) */
         addBtn(header)
         clearInterval(interval)
     }
 },1000)
 
-function addBtn(header) {
+// Botão para abrir menu
+const addBtn = header => {
     const btn = document.createElement('div')
     btn.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
@@ -17,106 +17,131 @@ function addBtn(header) {
     btn.setAttribute('id', 'btn')
     btn.setAttribute('class', 'PVMjB')
     header.appendChild(btn)
-    /* console.log('oentrei ta') */
 
     addDivMenu(btn)
-    const divMenu = document.querySelector('#menu')
-    addUl(divMenu)
-    hideShowMenu(btn, divMenu)
-    closeMenu(btn, divMenu)
 }
 
 // Adicionar div para escolher o playbackRate
-function addDivMenu (element) {
+const addDivMenu = element => {
     const divSelectSpeed = document.createElement('div')
     divSelectSpeed.setAttribute('id', 'menu')
     divSelectSpeed.setAttribute('class', '_2s_eZ _1bC39')
     divSelectSpeed.setAttribute('style', 'transform-origin: right top; transform: scale(1); opacity: 1;')
 
-    /* divSelectSpeed.innerHTML =
-    `
-    <ul class="I4jbF">
-        <li tabindex="-1" class="_1N-3y eP_pD _36Osw" data-animate-dropdown-item="true" style="opacity: 1;">
-            <div class="Ut_N0 n-CQr" role="button" title="Novo grupo">0.25x</div>
-        </li>
-        <li tabindex="-1" class="_1N-3y eP_pD _36Osw" data-animate-dropdown-item="true" style="opacity: 1;">
-            <div class="Ut_N0 n-CQr" role="button" title="Criar uma sala">0.5</div>
-        </li>
-        <li tabindex="-1" class="_1N-3y eP_pD _36Osw" data-animate-dropdown-item="true" style="opacity: 1;">
-            <div class="Ut_N0 n-CQr" role="button" title="Perfil">1x</div>
-        </li>
-        <li tabindex="-1" class="_1N-3y eP_pD _36Osw" data-animate-dropdown-item="true" style="opacity: 1;">
-            <div class="Ut_N0 n-CQr" role="button" title="Arquivadas">1.5x</div>
-        </li>
-        <li tabindex="-1" class="_1N-3y eP_pD _36Osw" data-animate-dropdown-item="true" style="opacity: 1;">
-            <div class="Ut_N0 n-CQr" role="button" title="Favoritas">2x</div>
-        </li>
-    </ul>
-    ` */
-
     element.appendChild(divSelectSpeed)
-    /* console.log(divSelectSpeed) */
+
+    const divMenu = document.querySelector('#menu')
+    hideShowMenu(element, divMenu)
 }
 
-function addUl(element) {
-    let options = [
-        "0.25x", 
-        "0.5x", 
-        "1x", 
-        "1.5x",
-        "2x"
-    ]
-    let titles = [
-        "Velocidade 0.25",
-        "Velocidade 0.5",
-        "Velocidade 1",
-        "Velocidade 1.5",
-        "Velocidade 2"
+// Adicionar itens no menu
+const insertItemsIntoMenu = element => {
+    const options = [
+        speeds = [
+            "0.25x", 
+            "0.5x", 
+            "1x", 
+            "1.5x",
+            "2x"
+        ],
+        titles= [
+            "Velocidade 0.25",
+            "Velocidade 0.5",
+            "Velocidade 1",
+            "Velocidade 1.5",
+            "Velocidade 2"
+        ],
+        values = [
+            0.25,
+            0.5,
+            1,
+            1.5,
+            2
+        ]
     ]
 
+    insertUlIntoMenu(element, options)
+}
+
+// Criar ul
+const insertUlIntoMenu = (element, values) => {
     const ul = document.createElement('ul')
     ul.setAttribute('class', 'I4jbF')
     element.appendChild(ul)
 
-    for(let i = 0; i < options.length; i++) {
-        // Crie o item da lista:
+    insertLiIntoUl(ul, values)
+}
+
+// Criar li
+const insertLiIntoUl = (element, values) => {
+    for(let i = 0; i < values[0].length; i++) {
+        // Criando a li
         const li = document.createElement('li')
         li.setAttribute('tabindex','-1')
         li.setAttribute('class', '_1N-3y eP_pD _36Osw')
         li.setAttribute('data-animate-dropdown-item', 'true')
         li.setAttribute('style', 'opacity: 1')
-        
-        // Defina seu conteúdo:
-        
-        //class="Ut_N0 n-CQr" role="button" title="Novo grupo"
+
+        // Criando o item da li:
         const div = document.createElement('div')
         div.setAttribute('class','Ut_N0 n-CQr')
         div.setAttribute('role','button')
-        div.setAttribute('title', titles[i])
-        
-        div.appendChild(document.createTextNode(options[i]));
+        div.setAttribute('title', values[1][i])
+        div.setAttribute('value', values[2][i])
+        let text = document.createTextNode(values[0][i])
+        div.appendChild(text)
 
-        // Adicione-o à lista:
-        ul.appendChild(li)
+        element.appendChild(li)
         li.appendChild(div)
+
+        hoverItem(i)
+        pegarValor(div, values[2][i])
     }
 }
-
-// Mostrar/Ocultar divPopup(menu)
-function hideShowMenu(elementBtn, elementDivMenu) {
-    elementBtn.addEventListener('click', () => {
-        if(elementDivMenu.style.display == 'none') {
-            elementDivMenu.style.display = 'block'
-            elementBtn.classList.add('_4QpsN')
-        } else {
-            elementDivMenu.style.display = 'none'
-            elementBtn.classList.remove('_4QpsN')
+////////////////////////////////////
+const pegarValor = (element, values) => {
+    element.addEventListener('click', (e) => {
+        if(e.target.getAttribute('class') == 'Ut_N0 n-CQr') {
+            let playbackRate = e.target.value = values
+            localStorage.setItem('playbackRate', playbackRate)
         }
     })
 }
 
+
+
+// Adicionar efeito hover nos items
+const hoverItem = value => {
+    const lists = document.querySelectorAll('li')
+    console.log(lists)
+    lists[value].addEventListener('mouseenter', () => {
+        lists[value].classList.add('_39Nmx')
+        
+    })
+
+    lists[value].addEventListener('mouseout', () => {
+        lists[value].classList.remove('_39Nmx')
+    })
+}
+
+// Mostrar/Ocultar divPopup(menu)
+const hideShowMenu = (elementBtn, elementDivMenu) => {
+    elementBtn.addEventListener('click', () => {
+        if(elementDivMenu.style.display == 'none') {
+            elementDivMenu.style.display = 'block'
+            elementBtn.classList.add('_4QpsN')
+            return
+        }
+        elementDivMenu.style.display = 'none'
+        elementBtn.classList.remove('_4QpsN')
+    })
+
+    insertItemsIntoMenu(elementDivMenu)
+    closeMenu(elementBtn, elementDivMenu)
+}
+
 // Fechar div ao clicar fora
-function closeMenu(elementBtn, elementDivMenu) {
+const closeMenu = (elementBtn, elementDivMenu) => {
     window.addEventListener('click', (e) => {
         if(e.target.id != 'menu' && e.target.id != 'btn') {
           elementDivMenu.style.display = 'none'
@@ -126,7 +151,7 @@ function closeMenu(elementBtn, elementDivMenu) {
 }
 
 // Aqui eu vou adicionar o playbackRate
-function changePlaybackRate() {
+const changePlaybackRate = () => {
     window.addEventListener('click', (e) => {
         if(e.target.getAttribute('data-testid') == 'audio-play') {
             console.log('Dei play')
@@ -137,6 +162,6 @@ function changePlaybackRate() {
 
 // Organizar código [x]
 // Organizar nome das variáveis [x]
-// Criar ul dentro da div popup
-// Configurar opções e salvar no localStorange
+// Criar ul dentro da div popup [x]
+// Configurar opções e salvar no localStorange[x]
 // Criar função para playbackRate
